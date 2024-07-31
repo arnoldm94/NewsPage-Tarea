be647e62888fbc963c0f8ec7675cfdc4d02b2768
Arnold Melet <arnoldmelet18@gmail.com>
2024-07-31 21:25:09 +0200

    loader en lista de noticias

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className="card text-center">
-  <div className="card-header">
-    Footer
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">Nombre de la Empresa</h5>
-    <p className="card-text">Puedes buscarnos en las redes sociales, y nuestro horario de atencion es de Lunes a Viernes</p>
-    
-  </div>
-  <div className="card-footer text-body-secondary">
-    Designed by: Arnold Melet
-  </div>
-</div>
-  )
-}
+    <div id="footernews" className="card text-center">
+      <div className="card-header">Footer</div>
+      <div className="card-body">
+        <h5 className="card-title">Nombre de la Empresa</h5>
+        <p className="card-text">
+          Puedes buscarnos en las redes sociales, y nuestro horario de atencion es de Lunes a
+          Viernes
+        </p>
+      </div>
+      <div className="card-footer text-body-secondary">Designed by: Arnold Melet</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/views/List.jsx
+++ src/components/views/List.jsx
@@ -1,30 +1,42 @@
 import axios from "axios";
 import React from "react";
-import Article from './Article'
+import Article from "./Article";
 import { useState, useEffect } from "react";
 
+const List = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    getArticles();
+  }, []);
+  const getArticles = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=lszIMKm6mmngIqA7qAxgPX0LCpRiUpXR"
+      );
+      const loader = document.getElementById("loaded");
+      loader.className = "container loaded";
+      setArticles(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <h1 className="titlelist">Lista de noticias</h1>
+      <div className="container" id="loaded">
+        <div className="cargando">
+          <div className="pelotas"></div>
+          <div className="pelotas"></div>
+          <div className="pelotas"></div>
+          <span className="texto-cargando">Cargando...</span>
+        </div>
+      </div>
 
-const List =  () => {
- 
-const [articles, setArticles] = useState([])
-useEffect(() => {
-getArticles()
-}, [])
-const getArticles = async () => {
-try {
-const response = await axios.get('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=lszIMKm6mmngIqA7qAxgPX0LCpRiUpXR')
-setArticles(response.data.results)
-} catch (error) { console.error(error) }
-}
-return (
-<>
-<h1 className="titlelist">Lista de noticias</h1>
-{articles.map((article, index) => (
-<Article key={index} data={article} />
-))}
-</>
-)
-}
-
+      {articles.map((article, index) => (
+        <Article key={index} data={article} />
+      ))}
+    </>
+  );
+};
 
 export default List;
